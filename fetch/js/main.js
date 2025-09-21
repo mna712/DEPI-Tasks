a62f69304830118b3cc8bd72ad9cb584d4413fde
Menna Elgharabawii <162707844+mna712@users.noreply.github.com>
2025-09-21 04:11:10 +0300

    3 ways to fetch an API

--- fetch/js/main.js
+++ fetch/js/main.js
@@ -1,33 +1,85 @@
-function fetchdata() {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function () {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      var response = JSON.parse(xmlHttp.responseText);
-      var recipes = response.recipes;
-      var content = "";
+// function fetchdata() {
 
-      recipes.forEach((e) => {
-        var box = `
-          <div class="col-md-4 text-center mb-5" >
-            <img src="${e.image_url}"class="w-100" style="height:350px">
-            <h6>${e.title}</h6>
-            <h5>${e.publisher}</h5>
-            <h5>${e.recipe_id}</h5>
-          </div> 
-        `;
-        content += box;
-      });
+// ==================
+//       method 1
+//   =================
 
-      document.querySelector(".test").innerHTML = content;
-    }
-  };
-  xmlHttp.open(
-    "GET",
-    "https://forkify-api.herokuapp.com/api/search?q=pizza",
-    true
+// var xmlHttp = new XMLHttpRequest();
+// xmlHttp.onreadystatechange = function () {
+//   if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+//     var response = JSON.parse(xmlHttp.responseText);
+//     var recipes = response.recipes;
+//     var content = "";
+//     recipes.forEach((e) => {
+//       var box = `
+//         <div class="col-md-4 text-center mb-5" >
+//           <img src="${e.image_url}"class="w-100" style="height:350px">
+//           <h6>${e.title}</h6>
+//           <h5>${e.publisher}</h5>
+//           <h5>${e.recipe_id}</h5>
+//         </div>
+//       `;
+//       content += box;
+//     });
+//     document.querySelector(".test").innerHTML = content;
+//   }
+// };
+// xmlHttp.open(
+//   "GET",
+//   "https://forkify-api.herokuapp.com/api/search?q=pizza",
+//   true
+// );
+// xmlHttp.send();
+
+// ==================
+//       method 2
+//   ================
+
+// fetch("https://forkify-api.herokuapp.com/api/search?q=pizza")
+//   .then((res) => res.json())
+//   .then((response) => {
+//     var recipes = response.recipes;
+//     var content = "";
+//     recipes.forEach((e) => {
+//       var box = `
+//         <div class="col-md-4 text-center mb-5" >
+//           <img src="${e.image_url}"class="w-100" style="height:350px">
+//           <h6>${e.title}</h6>
+//           <h5>${e.publisher}</h5>
+//           <h5>${e.recipe_id}</h5>
+//         </div>
+//       `;
+//       content += box;
+//     });
+//     document.querySelector(".test").innerHTML = content;
+//   });
+// }
+
+// ==================
+//       method 3
+//   ===================
+
+async function fetchdata() {
+  var response = await fetch(
+    "https://forkify-api.herokuapp.com/api/search?q=pizza"
   );
-  xmlHttp.send();
+  var data = await response.json();
+  var recipes = data.recipes;
+  var content = "";
+  recipes.forEach((e) => {
+    var box = `
+        <div class="col-md-4 text-center mb-5" >
+          <img src="${e.image_url}"class="w-100" style="height:350px">
+          <h6>${e.title}</h6>
+          <h5>${e.publisher}</h5>
+          <h5>${e.recipe_id}</h5>
+        </div>
+      `;
+    content += box;
+  });
+  document.querySelector(".test").innerHTML = content;
 }
 
-fetchdata();
+// }
 
+fetchdata();
